c31222cc2197770021422372ec92ad2d3cc1c362
marielledoenges <129237039+marielledoenges@users.noreply.github.com>
2024-04-10 16:49:03 -0400

    Add files via upload
    creating a class to handle all of the data writing between file and code use.

---
+++ generalData.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class worldInfo{
+private:
+    vector<string> cityNames;
+    vector<string> months = {"jan", "feb", "mar", "apr",
+                             "may", "jun", "jul", "aug",
+                             "sept", "oct", "nov", "dec"};
+
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "dataWriting.h"
+using namespace std;
+
+int main() {
+
+    return 0;
+}
