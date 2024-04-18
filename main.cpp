74b277a9b6c67456d6264ac379fe0cd9cd2f5923
marielledoenges <129237039+marielledoenges@users.noreply.github.com>
2024-04-17 21:29:16 -0400

    main function for writing csv data

--- main.cpp
+++ main.cpp
@@ -3,10 +3,16 @@
 #include <vector>
 #include <fstream>
 #include "generalData.h"
+#include "flight.h"
+#include <algorithm>
+#include <map>
 using namespace std;
 
+
 int main() {
-    WorldInfo temp;
-    temp.writeFile("hi");
+
+    WorldInfo test;
+    test.writeFile("proj3DataTest.csv");
+
     return 0;
 }
