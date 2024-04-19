ee59801454b65c0a12a9fc6c80f6d4d3c5cea09a
eneida-maria-escobar <116766094+eneida-maria-escobar@users.noreply.github.com>
2024-04-19 18:39:33 -0400

    Update FrontEnd.h

--- FrontEnd.h
+++ FrontEnd.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "Flight.h"
+#include <vector>
 
 struct FrontEnd {
     sf::RenderWindow window;
@@ -9,9 +10,13 @@ struct FrontEnd {
     sf::Color textColor;
     sf::Color outlineColor;
 
+    // Constructor
     FrontEnd(int width, int height, const std::string& title);
+
+    // Methods
     void run();
     void loadFont(const std::string& fontPath);
     void drawBoardingPass(const Flight& flight);
     void handleUserInput(std::vector<Flight>& availableFlights, Flight& selectedFlight);
 };
+
