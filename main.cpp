f5ac54e84d335df1784da468db79ff4f0fc4db37
eneida-maria-escobar <116766094+eneida-maria-escobar@users.noreply.github.com>
2024-04-19 18:35:29 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,8 +15,11 @@ int main() {
     WorldInfo test;
     test.writeFile("proj3DataTest.csv");
 
+   // Initialize your application interface
     FrontEnd app(800, 600, "Boarding Pass Viewer");
-    app.loadFont(".ttf");  
+    // Load the Roboto font
+    app.loadFont("fonts/Roboto-Regular.ttf");  // Ensure the path is correct
+    // Run the application
     app.run();
 
     return 0;
