957a76a0c3e7307524bc2b5a0de3233d06f6f4ca
MaggieSnead <100615753+MaggieSnead@users.noreply.github.com>
2024-04-18 21:03:40 -0400

    More changes to match new implementation

--- Graph.h
+++ Graph.h
@@ -8,15 +8,11 @@
 #include <unordered_map>
 using namespace std;
 
-struct City{    //FIXME not sure if needed
-    string name, country;
-    City(string &cityName, string &countryName) : name(cityName), country(countryName){}
-};
-
-// Represents an individual graph, holding all relevant details and providing methods for accessing these details.
 struct Flight {
     string originCity;    // The departure city of the graph.
+    string originCountry;
     string destinationCity;   // The destination city of the graph.
+    string destCountry;
     string time;     // Descriptive time of departure (e.g., "Morning", "Afternoon", "Evening").
     string month;
     string date;
@@ -28,11 +24,11 @@ struct Flight {
     bool intl;
 
     //constructor of our flight object
-    Flight(string &cityFrom, string &cityTo, int &priceGiven, int &distanceGiven,
+    Flight(string &cityFrom, string &countryFrom,string &cityTo, string &countryTo,int &priceGiven, int &distanceGiven,
            int &durationGiven, float &timeZoneDiff, string &monthGiven, string &dateGiven, string &departureTime,
            bool &international, string &number):
 
-            originCity(cityFrom), destinationCity(cityTo), price(priceGiven), distance(distanceGiven),
+            originCity(cityFrom), originCountry(countryFrom), destinationCity(cityTo), destCountry(countryTo), price(priceGiven), distance(distanceGiven),
             duration(durationGiven), time(departureTime), month(monthGiven), date(dateGiven),
             timeDiff(timeZoneDiff), flightNumber(number), intl(international)
     {}
@@ -42,6 +38,10 @@ struct Flight {
 
 class Graph{
 private:
+    set<string> globalCities;     //keeps track of what cities have already been added
+   //map<string, Flight*> flightPair;        //maps a specific flight number with a specific flight
+    Flight* g[100][100];     //graph representation of matrix
+
     unordered_map<string, int> cityNamesMap = {{"Tampa", 0}, {"Gainesville", 1}, {"Paris", 2}, {"London", 3}, {"New York", 4},
                                                {"Miami", 5}, {"Washington DC", 6}, {"Seattle", 7}, {"Las Vegas", 8}, {"Los Angeles", 9},
                                                {"Houston", 10}, {"Austin", 11}, {"Dallas", 12}, {"Atlanta", 13}, {"Charleston", 14},
@@ -63,32 +63,17 @@ private:
                                                {"Abuja", 90}, {"Nairobi", 91}, {"Zanzibar", 92}, {"Cape Town", 93}, {"Prague",94},
                                                {"San Fransisco", 95}, {"Honolulu", 96}, {"Santorini", 97}, {"Madeira", 98}, {"Oporto", 99}};
 
-    set<string> globalCities;     //keeps track of what cities have already been added
-    map<string, Flight*> flightPair;        //maps a specific flight number with a specific flight
-    Flight* g[100][100];     //graph representation of matrix FIXME initialize all matrix entries with " " our graph map will store graph numbers
+    //Flight* createFlightHelper(string &cityFrom, string &countryFrom, string &cityTo, string &countryTo, string &price,string &distance, string &duration, string &timeZoneDiff,
+     //                          string &month, string &date, string &departureTime, string &international, string &flightNum);    //create map helper
 
-
-    City* createCityHelper(string &cityName, string &countryName);
-    Flight* createFlightHelper(string &cityFrom, string &cityTo, string &price,string &distance, string &duration, string &timeZoneDiff,
-                               string &month, string &date, string &departureTime, string &international, string &flightNum);    //create map helper
-
-    string cheapestDirect(string &origin);     //given a starting city, returns cheapest direct flight number
-    string directExists(string &origin, string &dest);        //returns flightNum if a direct graph exists between two cities
-    string flightMonth(string &origin, string  &month);        //returns the flight number of cheapest flight in a given month, returns empty if no flight exists
-    vector<string> cheapestPath(string &origin, string &dest);        //cheapest way to get between two places if possible, returns a vector of flight numbers. should be generalized
+    Flight* cheapestDirect(string &origin);     //given a starting city, returns cheapest direct flight number
+    Flight* directExists(string &origin, string &dest);        //returns flightNum if a direct graph exists between two cities
+    Flight* flightMonth(string &origin, string  &month);        //returns the flight number of cheapest flight in a given month, returns empty if no flight exists
+    vector<Flight*> cheapestPath(string &origin, string &dest);        //cheapest way to get between two places if possible, returns a vector of flight numbers. should be generalized
     bool international(string &origin, string &dest);    //returns if the graph is international or not
 
 public:
     Flight* current;
-    City* myCity;
-
-    // Getter methods to expose private properties in a controlled manner.
-    /*string getOriginCity() const;
-    string getDestinationCity() const;
-    int getPrice() const;
-    int getDuration() const;
-    int getStops() const;
-    string getTimeOfDay() const; */
 
     Graph(){
         for(int i = 0; i < 100; i++){
@@ -102,7 +87,7 @@ public:
                    string month, string date, string departureTime, string international, string flightNum); //creating actual map
 
     void getBest(string& filter, string& origin, string& dest);        //returns the best flight based on the filter passed in
-    void printBoardingPass(string& filter, string& flightNum); //prints the graph based off of filter and the best graph
+    void printBoardingPass(string& filter, Flight* thisFlight); //prints the graph based off of filter and the best graph
     void readCSVFile(string filename);
-    int i(string city);
+    int n(string city);
 };
