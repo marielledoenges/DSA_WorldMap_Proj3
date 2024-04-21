93bb8f4f3da9e9938a6976f8a3c8ee003b41b7bb
marielledoenges <129237039+marielledoenges@users.noreply.github.com>
2024-04-21 18:03:03 -0400

    texture manager

---
+++ TextureManager.cpp
@@ -0,0 +1,22 @@
+#include <unordered_map>
+#include "SFML/Graphics.hpp"
+#include "TextureManager.h"
+
+unordered_map<string, sf::Texture> TextureManager::textures;
+
+sf::Texture& TextureManager::getTexture(std::string textureName) {
+    auto result = textures.find(textureName);
+    if(result == textures.end()) {
+        // Texture does not already exist in the map, go get it!
+        sf::Texture newTexture;
+        newTexture.loadFromFile("images/" + textureName + ".jpg");
+        textures[textureName] = newTexture;
+        return textures[textureName];
+    }
+    else {
+        // Texture already exists, return it!
+        return result->second;
+    }
+
+}
+
